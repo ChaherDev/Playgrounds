a1dbd0e4964bbb7c080867efd37ef6a2491934b1
Chaher <chaher.croissance.public@gmail.com>
2024-08-24 13:27:23 +0200

    How to create variables and constants

--- Day 1.playground/Contents.swift
+++ Day 1.playground/Contents.swift
@@ -1,3 +1,27 @@
 import Cocoa
 
 var greeting = "Hello, playground"
+
+var name = "Ted"
+name = "Robecca"
+name = "Keeley"
+
+let character = "Daphne"
+
+// We can't change the value of character
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+// For variables, we use camelCase (convention)
+
+let managerName = "Michael Scott"
+let dogBreed = "Samoyed"
+let meaningOgLife = "How many roads must a man walk down?"
+
