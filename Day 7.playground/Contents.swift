64bf1741be4079702a01267c0ecc66e9b84ddb35
Chaher Machhour <chaher.croissance.public@gmail.com>
2024-09-02 15:08:30 +0200

    Day 7 completed

--- Day 7.playground/Contents.swift
+++ Day 7.playground/Contents.swift
@@ -1,3 +1,88 @@
 import Cocoa
 
 var greeting = "Hello, playground"
+
+func showWelcome() {
+    print("Welcome to my app!")
+    print("By default This prints out a conversion")
+    print("chart from centimeters to inches, but you")
+    print("can also set a custome range if you want.")
+}
+
+showWelcome()
+
+func printTimesTables(number: Int, end: Int) {
+    for i in 1...end {
+        print("\(i) * \(number) is \(i * number)")
+    }
+}
+
+printTimesTables(number: 5, end: 20)
+
+let root = sqrt(169)
+print(root)
+
+func rollDice() -> Int {
+    return Int.random(in: 1...6)
+}
+
+let result = rollDice()
+print(result)
+
+let firstString = "abc"
+let secondString = "bca"
+
+func checkStrings(first: String, second: String) -> Bool {
+    if firstString.sorted() == secondString.sorted() {
+        return true
+    } else {
+        return false
+    }
+}
+
+func pythagoras(a: Double, b: Double) -> Double {
+    let input = a * a + b * b
+    let root = sqrt(input)
+    return root
+}
+
+let c = pythagoras(a: 3, b: 4)
+print(c)
+
+// Tuples
+
+func isUppercase(string: String) -> Bool {
+    string == string.uppercased()
+}
+
+func getUser() -> (firstName: String, lastName: String) {
+    (firstName: "Taylor", lastName: "Swift")
+}
+
+let user = getUser()
+
+print("Name: \(user.firstName) \(user.lastName)")
+
+func getUser2() -> (firstName: String, lastName: String) {
+    ("Taylor", "Swift")
+}
+
+let user2 = getUser2()
+
+print("Name: \(user2.firstName) \(user2.lastName)")
+print("Name: \(user2.0) \(user2.1)")
+
+func isUppercase(_ string: String) -> Bool {
+    string == string.uppercased()
+}
+
+let string = "HELLO, WORLD"
+let newResult = isUppercase(string)
+
+func printTimesTables(for number: Int) {
+    for i in 1...12 {
+        print("\(i) x \(number) is \(i * number)")
+    }
+}
+
+printTimesTables(for: 5)
