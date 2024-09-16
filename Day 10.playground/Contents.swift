595c01b41bcc026970cbd2122c160da1b4d2518c
Chaher Machhour <chaher.croissance.public@gmail.com>
2024-09-16 16:14:35 +0200

    How to take action when a property changes

--- Day 10.playground/Contents.swift
+++ Day 10.playground/Contents.swift
@@ -79,4 +79,23 @@ superArcher.vacationTaken += 4
 superArcher.vacationRemaining = 5
 print(superArcher.vacationAllocated)
 
-// 
+// How to take action when a property changes
+
+struct App {
+    var contacts = [String]() {
+        willSet {
+            print("Current value is: \(contacts)")
+            print("New value will be: \(newValue)")
+        }
+        
+        didSet {
+            print("There are now \(contacts.count) contacts")
+            print("Old value was: \(oldValue)")
+        }
+    }
+}
+
+var app = App()
+app.contacts.append("Adrian E")
+app.contacts.append("Allen W")
+app.contacts.append("Ish S")
