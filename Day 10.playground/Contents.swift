b06579acf647f6164c7308aca331aa0bbca2d2ec
Chaher Machhour <chaher.croissance.public@gmail.com>
2024-09-16 16:39:20 +0200

    Custom initializers

--- Day 10.playground/Contents.swift
+++ Day 10.playground/Contents.swift
@@ -99,3 +99,30 @@ var app = App()
 app.contacts.append("Adrian E")
 app.contacts.append("Allen W")
 app.contacts.append("Ish S")
+
+// Custom initializers
+
+struct Player {
+    let name: String
+    let number: Int
+    
+    init(name: String, number: Int) {
+        self.name = name
+        self.number = number
+    }
+}
+
+let player = Player(name: "Megan R", number: 15)
+
+struct SuperPlayer {
+    let name: String
+    let number: Int
+    
+    init(name: String) {
+        self.name = name
+        number = Int.random(in: 1...99)
+    }
+}
+
+let superPlayer = SuperPlayer(name: "Megan R")
+print(player.number)
