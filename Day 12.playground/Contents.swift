c01c282512eb2091f5a946e5aef385397a0906e8
Chaher Machhour <chaher.croissance.public@gmail.com>
2024-09-18 11:35:05 +0200

    Class initializers

--- Day 12.playground/Contents.swift
+++ Day 12.playground/Contents.swift
@@ -49,3 +49,23 @@ joseph.work()
 let novall = Developer(hours: 8)
 novall.printSummary()
 
+// Class initializers
+
+class Vehicle {
+    let isElectric: Bool
+    
+    init(isElectric: Bool) {
+        self.isElectric = isElectric
+    }
+}
+
+class Car: Vehicle {
+    let isConvertible: Bool
+    
+    init(isElectric: Bool, isConvertible: Bool) {
+        self.isConvertible = isConvertible
+        super.init(isElectric: isElectric)
+    }
+}
+
+let teslaX = Car(isElectric: true, isConvertible: false)
