a9cbfa96dd497a9da99c19abc885d8ff58f41849
Chaher Machhour <chaher.croissance.public@gmail.com>
2024-09-20 18:31:49 +0200

    How to unwrap optionals with nil coalescing ?

--- Day 14.playground/Contents.swift
+++ Day 14.playground/Contents.swift
@@ -46,3 +46,30 @@ func printSquare(of number: Int?) {
     
     print("\(number) x \(number) is \(number * number)")
 }
+
+// How to unwrap optionals with nil coalescing ?
+
+let captains = [
+    "Entreprise": "Picard",
+    "Voyager": "Janneway",
+    "Defiant": "Sisko"
+]
+
+let new = captains["Serenity"] ?? "N/A"
+
+let tvShows = ["Archer", "Babylon 5", "Ted Lasso"]
+let favorite = tvShows.randomElement() ?? "None"
+
+struct Book {
+    let title: String
+    let author: String?
+}
+
+let book = Book(title: "Beowulf", author: nil)
+let author = book.author ?? "Anonymous"
+print(author)
+
+let input = ""
+let againNumber = Int(input) ?? 0
+print(againNumber)
+
