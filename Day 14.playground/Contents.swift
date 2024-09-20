47a92821a3b5b4173717cb82e2a3bf397ccf7fd3
Chaher Machhour <chaher.croissance.public@gmail.com>
2024-09-20 19:19:52 +0200

    How to handle function failure with optionals

--- Day 14.playground/Contents.swift
+++ Day 14.playground/Contents.swift
@@ -88,3 +88,20 @@ struct BookAgain {
 var bookAgain: BookAgain? = nil
 let authorAgain = bookAgain?.author?.first?.uppercased() ?? "A"
 print(author)
+
+// How to handle function failure with optionals ?
+
+enum UserError: Error {
+    case badID, networkFailed
+}
+
+func getUser(id: Int) throws -> String {
+    throw UserError.networkFailed
+}
+
+if let user = try? getUser(id: 23) {
+    print("User: \(user)")
+}
+
+let user = (try? getUser(id: 23)) ?? "Anonymous"
+print(user)
